21b1a8b5d4843e997fa917e004c6bf46d92b81de
mfzhang412 <mfzhang@NN123-HA-19.HS.sd203.org>
2015-08-21 15:07:59 -0500

    Day 2

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -3,15 +3,38 @@ import java.awt.Color;
 public class TurtleWorld
 {
     public static void main(String args[])
+ throws InterruptedException
     {
         World turtleWorld = new World();
-        Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
-        turtle.penDown();
-        turtle.forward(50);
-        turtle.turnRight();
-        turtle.forward(50);
-        turtle.turnRight();
-        turtle.forward(50);
+        Turtle turtle1 = new Turtle(turtleWorld);
+        Turtle turtle2 = new Turtle(turtleWorld);
+        
+        Thread.sleep(1000);
+        turtle1.penUp();
+        turtle2.penUp();
+        turtle1.moveTo(250,250);
+        Thread.sleep(1000);
+        turtle2.moveTo(500,250);
+        Thread.sleep(1000);
+        turtle1.setPenColor(Color.RED);
+        turtle1.penDown();
+        turtle1.forward(50);
+        turtle1.turnRight();
+        turtle1.forward(50);
+        turtle1.turnRight();
+        turtle1.forward(50);
+        turtle1.turnRight();
+        turtle1.forward(50);
+        turtle1.setShellColor(Color.RED);
+        turtle2.setPenColor(Color.GREEN);
+        turtle2.penDown();
+        turtle2.forward(50);
+        turtle2.turnRight();
+        turtle2.forward(50);
+        turtle2.turnRight();
+        turtle2.forward(50);
+        turtle2.turnRight();
+        turtle2.forward(50);
+        turtle2.setShellColor(Color.GREEN);
     }
 }
