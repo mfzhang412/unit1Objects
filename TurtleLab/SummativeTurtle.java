6614e623d5b5cdfc8ecade952f334757dd0da075
mfzhang412 <mfzhang@NN123-HA-19.HS.sd203.org>
2015-09-03 14:56:41 -0500

    asdfasdf

--- TurtleLab/SummativeTurtle.java
+++ TurtleLab/SummativeTurtle.java
@@ -5,7 +5,9 @@ public class SummativeTurtle
     public static void main(String[] args)
     throws InterruptedException
     {
-        World turtleWorld = new World();
+        World turtleWorld = new World(735,551);
+        Picture picture = new Picture("C:\\Users\\mfzhang\\Downloads\\Turtle Image.jpg");
+        turtleWorld.setPicture(picture);
         Turtle turtle1 = new Turtle(turtleWorld);
         Turtle turtle2 = new Turtle(turtleWorld);
         Turtle turtle3 = new Turtle(turtleWorld);
@@ -21,13 +23,12 @@ public class SummativeTurtle
         turtle1.forward(100);
         turtle2.turn(180);
         turtle2.forward(100);
-        turtle3.forward(25);
-        turtle3.turn(-90);
+        turtle3.forward(75);
+        turtle3.turn(-120);
         turtle4.forward(25);
         turtle4.turn(90);
         turtle4.forward(100);
-
-
+        
         int loopVal;
         for (loopVal = 0; loopVal < 126; loopVal++) {
             turtle1.turn(-90);
@@ -61,7 +62,7 @@ public class SummativeTurtle
             turtle1.setPenColor(Color.YELLOW);
             turtle2.setPenColor(Color.CYAN);
             turtle3.setPenColor(Color.lightGray);
-            turtle4.setPenColor(Color.PINK);
+            turtle4.setPenColor(Color.RED);
             turtle1.forward(100);
             turtle2.forward(100);
             turtle3.forward(150);
